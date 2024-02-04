249dd726397082774d638ca2e17537f93151560f
AndrewQuijano <andrewquijano92@gmail.com>
2024-02-04 17:47:18 -0500

    pulled out BigIntPoint class.

--- src/main/java/HomomorphicPaths.java
+++ src/main/java/HomomorphicPaths.java
@@ -9,49 +9,41 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.nio.charset.StandardCharsets;
 
+// Starting with this:
+// https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+
 public class HomomorphicPaths {
-
-     public static class BigIntPoint
-     {
-          BigInteger x;
-          BigInteger y;
-
-          public BigIntPoint(BigInteger x, BigInteger y)
-          {
-               this.x = x;
-               this.y = y;
-          }
-     }
 
      public static BigInteger maxBigInt(BigInteger a, BigInteger b)
      {
-          if(a.compareTo(b) == -1) return b;
-          else return a;
+          if(a.compareTo(b) < 0) {
+               return b;
+          }
+          else {
+               return a;
+          }
 
      }
      public static BigInteger minBigInt(BigInteger a, BigInteger b)
      {
-          if(a.compareTo(b) == -1) return a;
-          else return b;
-
+          if(a.compareTo(b) < 0) {
+               return a;
+          }
+          else {
+               return b;
+          }
      }
 
      public static boolean geBigInt(BigInteger a, BigInteger b) {
-          if (a.compareTo(b) == -1) return false;
-          else return true;
+         return a.compareTo(b) >= 0;
 
      }
-     public static boolean leBigInt(BigInteger a, BigInteger b){
-          if(a.compareTo(b) == 1) return true;
-          else return false;
+     public static boolean leBigInt(BigInteger a, BigInteger b) {
+         return a.compareTo(b) > 0;
      }
-     static boolean onSegment(BigIntPoint p, BigIntPoint q, BigIntPoint r)
-     {
-          if (leBigInt(q.x,(maxBigInt(p.x, r.x))) && geBigInt(q.x,(minBigInt(p.x,r.x)))
-                  && leBigInt(q.y,(maxBigInt(p.y,r.y))) && leBigInt(q.y,(minBigInt(p.y,r.y))))
-               return true;
-
-          return false;
+     static boolean onSegment(BigIntPoint p, BigIntPoint q, BigIntPoint r) {
+         return leBigInt(q.x, (maxBigInt(p.x, r.x))) && geBigInt(q.x, (minBigInt(p.x, r.x)))
+                 && leBigInt(q.y, (maxBigInt(p.y, r.y))) && leBigInt(q.y, (minBigInt(p.y, r.y)));
      }
 
      static int orientation(BigIntPoint p, BigIntPoint q, BigIntPoint r)
@@ -66,11 +58,17 @@ public class HomomorphicPaths {
 
           int ans = val.compareTo(BigInteger.ZERO);
 
-          if (ans==0) return 0;
+          if (ans == 0) {
+               return 0;
+          }
 
-          else if (ans == 1) return 1;
+          else if (ans > 0) {
+               return 1;
+          }
 
-          else return 2;
+          else {
+               return 2;
+          }
      }
 
      static boolean doIntersect(BigIntPoint p1, BigIntPoint q1, BigIntPoint p2, BigIntPoint q2)
@@ -80,13 +78,22 @@ public class HomomorphicPaths {
           int o3 = orientation(p2,q2,p1);
           int o4 = orientation(p2,q2,q1);
 
-          if (o1 != o2 && o3 != o4)
+          if (o1 != o2 && o3 != o4) {
                return true;
+          }
 
-          if (o1 == 0 && onSegment(p1,p2,q1)) return true;
-          if (o2 == 0 && onSegment(p1,q2,q1)) return true;
-          if (o3 == 0 && onSegment(p2,p1,q2)) return true;
-          if (o4 == 0 && onSegment(p2,q1,q2)) return true;
+          if (o1 == 0 && onSegment(p1,p2,q1)) {
+               return true;
+          }
+          if (o2 == 0 && onSegment(p1,q2,q1)) {
+               return true;
+          }
+          if (o3 == 0 && onSegment(p2,p1,q2)) {
+               return true;
+          }
+          if (o4 == 0 && onSegment(p2,q1,q2)) {
+               return true;
+          }
 
           return false;
      }
@@ -139,14 +146,12 @@ public class HomomorphicPaths {
           ownroute = read_all_paths(ownroutefile);
           cryptroute = read_all_paths(cryptroutefile);
 
-          // TODO: Rewrite this to a Boolean, and place this on IntersectTest, on test_intersections
-          // Test for intersection
-          for (int i = 0; i < (ownroute.size()-1); i++){
+          // TODO: Rewrite this to a Boolean Function, with the inputs being both line segments and place this on IntersectTest, on test_intersections
+          for (int i = 0; i < (ownroute.size()-1); i++) {
                for (int j = 0; j < (cryptroute.size()-1); j++) {
                     if(doIntersect(ownroute.get(i),ownroute.get(i+1),cryptroute.get(j),cryptroute.get(j+1)))
                          System.out.println("Yes");
                }
           }
-
      }
 }
